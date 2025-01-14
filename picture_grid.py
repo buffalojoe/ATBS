7b3b3ffc1eb1ecadab135ff4fe37e9b1dc571115
buffalojoe <42217874+buffalojoe@users.noreply.github.com>
2025-01-14 18:59:31 -0500

    Create picture_grid.py

---
+++ picture_grid.py
@@ -0,0 +1,18 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+gridOutput = ''
+
+for i in range(0, len(grid[0])):
+    for j in range(0, len(grid)):
+        gridOutput += grid[j][i]
+    gridOutput += '\n'
+
+print(gridOutput)
